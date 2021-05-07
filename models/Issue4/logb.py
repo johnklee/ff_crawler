195a2d87a6ad60b64da57901d4a24a86e9ee9642
johnklee <puremonkey2007@gmail.com>
2021-05-07 14:26:17 +0800

    initialize Issue4

---
+++ models/Issue4/logb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import logging
+import os
+import coloredlogs
+
+
+################################
+# Constants
+################################
+MODU_PATH = os.path.dirname(__file__) if os.path.dirname(__file__) else './'
+''' Path of current module '''
+
+LOGGER_FORMAT = "%(threadName)s/%(levelname)s: <%(pathname)s#%(lineno)s> %(message)s"
+''' Format of Logger '''
+
+LOGGER_LEVEL = 20  # CRITICAL=50; ERROR=40; WARNING=30; INFO=20; DEBUG=10
+''' Message level of Logger '''
+
+
+################################
+# Constants
+################################
+def get_logger(name, level=LOGGER_LEVEL, fmt=LOGGER_FORMAT):
+    if 'purifier' in logging.root.manager.loggerDict:
+        return logging.getLogger('purifier')
+    else:
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        logger.propagate = False
+        coloredlogs.install(level=level, logger=logger, fmt=fmt)
+        return logger
